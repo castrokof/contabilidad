284b1f9be34847b0dc91209d9e5d515a49970567
castrokof <castrokof@gmail.com>
2023-03-12 23:41:05 -0500

    feat: add controller, views, migrations, rutes and model cuentasxpagar

--- resources/views/facturacion/cuentasxpagar/tabs/tabsIndexPayment.blade.php
+++ resources/views/facturacion/cuentasxpagar/tabs/tabsIndexPayment.blade.php
@@ -4,21 +4,21 @@
             <div class="card-header p-0 pt-1">
                 <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="custom-tabs-one-datos-del-paciente-tab" data-toggle="pill" href="#custom-tabs-one-datos-del-paciente" data-target="#custom-tabs-one-datos-del-paciente" role="tab" aria-controls="custom-tabs-one-datos-del-paciente" aria-selected="false">Registro de Pagos</a>
+                        <a class="nav-link active" id="custom-tabs-one-datos-del-resumenapagar-tab" data-toggle="pill" href="#custom-tabs-one-datos-del-resumenapagar" data-target="#custom-tabs-one-datos-del-resumenapagar" role="tab" aria-controls="custom-tabs-one-datos-del-resumenapagar" aria-selected="false">Registro de Pagos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="custom-tabs-one-historial-pagos-tab" data-toggle="pill" href="#custom-tabs-one-historial-pagos" data-target="#custom-tabs-one-historial-pagos" role="tab" aria-controls="custom-tabs-one-historial-pagos" aria-selected="false">Historial de Pagos</a>
+                        <a class="nav-link" id="custom-tabs-one-historial-detallepayment-tab" data-toggle="pill" href="#custom-tabs-one-historial-detallepayment" data-target="#custom-tabs-one-historial-detallepayment" role="tab" aria-controls="custom-tabs-one-historial-detallepayment" aria-selected="false">Historial de pagos</a>
                     </li>
                 </ul>
             </div>
             <div class="card-body">
                 <div class="tab-content" id="custom-tabs-one-tabContent">
-                    <div class="tab-pane fade active show" id="custom-tabs-one-datos-del-paciente" role="tabpanel" aria-labelledby="custom-tabs-one-datos-del-paciente-tab">
+                    <div class="tab-pane fade active show" id="custom-tabs-one-datos-del-resumenapagar" role="tabpanel" aria-labelledby="custom-tabs-one-datos-del-resumenapagar-tab">
                         <div class="card-body">
                             @include('facturacion.cuentasxpagar.form.formpayment')
                         </div>
                     </div>
-                    <div class="tab-pane fade " id="custom-tabs-one-historial-pagos" role="tabpanel" aria-labelledby="custom-tabs-one-historial-pagos-tab">
+                    <div class="tab-pane fade " id="custom-tabs-one-historial-detallepayment" role="tabpanel" aria-labelledby="custom-tabs-one-historial-detallepayment-tab">
                         <div class="card-body">
                             @include('facturacion.cuentasxpagar.tablas.tablaListPagos')
                         </div>
