16b5a88a73540cab398b2a41e9f83d7ce8033fb3
Carlos Bejarano <cabejarano21@gmail.com>
2023-04-29 12:41:52 -0500

    feat: se adiciona campo para asociar la factura que se genero al entregar el pendiente

--- resources/views/facturacion/cuentasxpagar/tabs/tabsIndexPayment.blade.php
+++ resources/views/facturacion/cuentasxpagar/tabs/tabsIndexPayment.blade.php
@@ -4,10 +4,14 @@
             <div class="card-header p-0 pt-1">
                 <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="custom-tabs-one-datos-del-resumenapagar-tab" data-toggle="pill" href="#custom-tabs-one-datos-del-resumenapagar" data-target="#custom-tabs-one-datos-del-resumenapagar" role="tab" aria-controls="custom-tabs-one-datos-del-resumenapagar" aria-selected="false">Registro de Pagos</a>
+                        <a class="nav-link active" id="custom-tabs-one-datos-del-resumenapagar-tab" data-toggle="pill"
+                        href="#custom-tabs-one-datos-del-resumenapagar" data-target="#custom-tabs-one-datos-del-resumenapagar"
+                        role="tab" aria-controls="custom-tabs-one-datos-del-resumenapagar" aria-selected="false">Registro de Pagos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="custom-tabs-one-historial-detallepayment-tab" data-toggle="pill" href="#custom-tabs-one-historial-detallepayment" data-target="#custom-tabs-one-historial-detallepayment" role="tab" aria-controls="custom-tabs-one-historial-detallepayment" aria-selected="false">Historial de pagos</a>
+                        <a class="nav-link" id="custom-tabs-one-historial-detallepayment-tab" data-toggle="pill"
+                        href="#custom-tabs-one-historial-detallepayment" data-target="#custom-tabs-one-historial-detallepayment"
+                        role="tab" aria-controls="custom-tabs-one-historial-detallepayment" aria-selected="false">Historial de pagos</a>
                     </li>
                 </ul>
             </div>
